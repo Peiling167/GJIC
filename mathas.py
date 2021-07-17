6a602e532b6e94233f7460826fff450be49c6d35
Peiling167 <peiling167@hotmail.co.uk>
2021-07-17 16:51:45 +0100

    add mathas.py

---
+++ mathas.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 17 16:36:27 2021
+
+@author: xthem3
+"""
+class mathxt:
+    
+    def __init__(self, p,q):
+        self.p=p
+        self.q=q
+    def subb(self):
+        return self.p-self.q
+    def addd(self):
+        return self.q+self.q
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
